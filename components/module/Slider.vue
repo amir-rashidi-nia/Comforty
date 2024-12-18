07679f60c079fbe2211d37a94b3e94b81dcfedda
amir <amir.rashidy78@gmail.com>
2024-12-18 14:41:05 +0330

    fix bug

--- components/module/Slider.vue
+++ components/module/Slider.vue
@@ -21,7 +21,7 @@
             <swiper-container class="swiper w-full pt-8"
                 :class="[ center ? 'container mx-auto px-4 xl:max-w-none xl:px-0' : 'container mx-auto px-4' ]" ref="containerRef" :autoHeight="true"
                 :breakpoints="breakpoints" :centeredSlides="center" :loop="center"  :mousewheel="{ invert: true }" :autoplay="{ delay: 3000, enabled: true }">
-                <swiper-slide v-for="item in items" :key="item?.id"  class="h-auto" :class="{ 'xl:slide' : center}">
+                <swiper-slide v-for="item in items" :key="item?.id"  class="h-auto" :class="{ 'slide' : center}">
                     <slot name="item" :item="item" />
                 </swiper-slide>
             </swiper-container>
